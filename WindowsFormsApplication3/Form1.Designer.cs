24ca21c050b8f8f4cb88dcae47e54f8169cedf61
JIorD <JIorDMI@mail.ru>
2019-03-02 23:15:45 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.отобразитьСкрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.contextMenuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Net";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.отобразитьСкрытьToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.выходToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(179, 54);
+            // 
+            // отобразитьСкрытьToolStripMenuItem
+            // 
+            this.отобразитьСкрытьToolStripMenuItem.Name = "отобразитьСкрытьToolStripMenuItem";
+            this.отобразитьСкрытьToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.отобразитьСкрытьToolStripMenuItem.Text = "Отобразить/Скрыть";
+            this.отобразитьСкрытьToolStripMenuItem.Click += new System.EventHandler(this.отобразитьСкрытьToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(175, 6);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(526, 259);
+            this.richTextBox1.TabIndex = 50;
+            this.richTextBox1.Text = "";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.richTextBox1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 290);
+            this.tableLayoutPanel1.TabIndex = 51;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 19);
+            this.flowLayoutPanel1.TabIndex = 52;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 290);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.MaximumSize = new System.Drawing.Size(540, 314);
+            this.MinimumSize = new System.Drawing.Size(40, 64);
+            this.Name = "Form1";
+            this.Text = "Net";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem отобразитьСкрытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int time = 60;      // время на восстановление связи
+
+        [DataContract]
+        public class Node
+        {
+            [DataMember]
+            public string Ip { get; set; }
+            [DataMember]
+            public string Name { get; set; }
+
+            public Button Btn { get; set; }
+
+            public int before_error, after_error;
+
+            public Node(string ip, string name)
+            {
+                Ip = ip;
+                Name = name;
+            }
+
+            public void addbtn(Button button)
+            {
+                Btn = button;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Node[] nodes;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataContractJsonSerializer jsonFormatterNode = new DataContractJsonSerializer(typeof(Node[]));
+            // читаем данные json
+            try
+            {
+                using (FileStream fs = new FileStream("net.json", FileMode.OpenOrCreate))
+                {
+                    nodes = (Node[])jsonFormatterNode.ReadObject(fs);
+
+                    int i = 16;
+                    foreach (Node node in nodes)
+                    {
+                        i += 21;
+                        //Создаем новую кнопку
+                        Button newbutton = new Button
+                        {
+                            Width = 15,
+                            Height = 15,
+                            BackColor = Color.Red
+                        };
+
+                        //Добавляем элемент на форму
+                        flowLayoutPanel1.Controls.Add(newbutton);
+                        //this.Controls.Add(newbutton);
+
+                        node.addbtn(newbutton);
+                        toolTip1.SetToolTip(node.Btn, node.Name);
+                        Console.WriteLine("Ip: {0} --- Node: {1}", node.Ip, node.Name);
+                    }
+
+                    this.MaximumSize = new Size(i, 540);
+                    this.MinimumSize = new Size(i, 40);
+                }
+            }
+            catch
+            {
+                if (DialogResult.OK == MessageBox.Show("Файл net.json поврежден либо отсутствует!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error))
+                    Application.Exit();
+            }
+
+            timer1.Start();
+
+            notifyIcon1.ShowBalloonTip(5, "Подсказка", "Нажмите, чтобы отобразить окно", ToolTipIcon.Info);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string error_node1;
+            error_node1 = null;
+            this.Text = "Net: " + DateTime.Now.ToString();
+            notifyIcon1.Text = "Net: " + DateTime.Now.ToString();
+
+            int max_error1 = 0;
+            foreach (Node node in nodes)
+            {
+                if (max_error1 < node.after_error)
+                    max_error1 = node.after_error;
+
+                if (node.after_error == 0)
+                    node.Btn.BackColor = Color.Green;
+                else
+                    node.Btn.BackColor = Color.Red;
+
+                if (time / (timer1.Interval / 1000) <= node.before_error && node.after_error == 0)
+                    richTextBox1.Text = DateTime.Now.ToString() + " - Связь с узлом " + node.Name + " восстановлена!\r\n" + richTextBox1.Text;
+
+                if (node.after_error * timer1.Interval / 1000 < time && time <= (node.after_error + 1) * timer1.Interval / 1000)
+                {
+                    error_node1 += "Нет связи с узлом " + node.Name + "!\r\n";
+                    richTextBox1.Text = DateTime.Now.ToString() + " - Нет связи с узлом " + node.Name + "!\r\n" + richTextBox1.Text;
+                }
+            }
+
+            if (time <= (max_error1 + 1) * timer1.Interval / 1000)
+                notifyIcon1.Icon = SystemIcons.Error;
+            else
+                notifyIcon1.Icon = Resource1.success;
+
+            if (!String.IsNullOrEmpty(error_node1))
+            {
+                notifyIcon1.BalloonTipIcon = ToolTipIcon.Error;
+                notifyIcon1.BalloonTipText = error_node1;
+                notifyIcon1.BalloonTipTitle = "Ошибка";
+                notifyIcon1.ShowBalloonTip(300);
+            }
+
+            if (backgroundWorker1.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void отобразитьСкрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            form_visible();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            form_visible();
+        }
+
+        private void form_visible()
+        {
+            if (this.Visible)
+                this.Hide();
+            else
+                this.Show();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions
+            {
+                // Use the default Ttl value which is 128,
+                // but change the fragmentation behavior.
+                DontFragment = true
+            };
+
+            // Create a buffer of 32 bytes of data to be transmitted.
+            string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+            byte[] buffer = Encoding.ASCII.GetBytes(data);
+            int timeout = 150;
+            PingReply reply;
+
+
+            foreach (Node node in nodes)
+            {
+                node.before_error = node.after_error;
+                try
+                {
+                    reply = pingSender.Send(node.Ip, timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)       // вместо всего if можно оставить еррор 0, во всех иных случиях будет срабатывать исключение
+                        node.after_error = 0;
+                    else
+                        node.after_error++;
+                }
+                catch
+                {
+                    node.after_error++;
+                }
+            }
+
+            /* if (reply.Status == IPStatus.Success)
+             {
+                 node_error[i, 0] = 0;
+                 //MessageBox.Show("Address: {0}" + reply.Address.ToString() + "RoundTrip time: {0}" + reply.RoundtripTime + "Time to live: {0}" + reply.Options.Ttl + "Don't fragment: {0}" + reply.Options.DontFragment + "Buffer size: {0}" + reply.Buffer.Length);
+                 //Console.WriteLine("Address: {0}", reply.Address.ToString());
+                 //Console.WriteLine("RoundTrip time: {0}", reply.RoundtripTime);
+                 //Console.WriteLine("Time to live: {0}", reply.Options.Ttl);
+                 //Console.WriteLine("Don't fragment: {0}", reply.Options.DontFragment);
+                 //Console.WriteLine("Buffer size: {0}", reply.Buffer.Length);
+             }
+             else
+             {
+                 node_error[i, 0]++;
+             }*/
+
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //e.Cancel = true;
+            //form_visible();
+        }
+    }
+}
